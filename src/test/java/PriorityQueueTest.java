b80c8a905c8cee2c26b1de1b3c177060f33cb23a
KennyLai <lk3567528@yahoo.com.tw>
2020-04-13 16:05:19 +0800

    fail the test

--- src/test/java/PriorityQueueTest.java
+++ src/test/java/PriorityQueueTest.java
@@ -72,7 +72,7 @@ public class PriorityQueueTest {
     @Test(expected = NullPointerException.class)
     public void testadd()
     {
-        pq.add(null);
+        pq.add(10);
     }
 
     @Test(expected = NullPointerException.class)
